718e7e1d439dd73156c5b8dc5dbab0ea9aeab65f
LauraRgz <lrodriguezs5@alumnos.nebrija.es>
2019-10-03 18:55:54 +0200

    commit

--- src/app.js
+++ src/app.js
@@ -1,32 +1,6 @@
 import request from 'request';
 import yargs from 'yargs';
 
-/*const url = 'https://api.darksky.net/forecast/af414fe762e5e219154cd7dd41438b01/40.39,-3.66?units=si&lang=es';
-
-
-request({ url: url, json: true }, (error, response) => {
-    console.log("Buenas tardes, estás en " + response.body.timezone);
-    console.log("La temperatura es: " + response.body.currently.temperature); // La info siempre está en body
-    console.log("La probabilidad de precipitación es: " + response.body.currently.precipProbability);
-});*/
- 
-// const baseURLDarksky = 'https://api.darksky.net/';
-// const tokenDarksky = 'af414fe762e5e219154cd7dd41438b01/';
-
-// const lat = 40.39;
-// const long = -3.66;
-// const parameters = '?units=si&lang=es';
-
-// const urlDarksky = `${baseURLDarksky}forecast/${tokenDarksky}${lat},${long}${parameters}`;
-
-// request({ url: urlDarksky, json: true }, (error, response) => {
-//     console.log("Buenas tardes, estás en " + response.body.timezone);
-//     console.log("La temperatura es: " + response.body.currently.temperature); // La info siempre está en body
-//     console.log("La probabilidad de precipitación es: " + response.body.currently.precipProbability);
-// });
-
-//https://api.mapbox.com/geocoding/v5/mapbox.places/vallecas.json?access_token=pk.eyJ1IjoibGF1cmFyZ3oiLCJhIjoiY2sxYW8xcG5uMTlldjNjbGw2aGNsYXZsNyJ9.yZNc4Be1agBMM6tVZ1uW-A
-
 const localization = function(argv){
     //Mapbox URL
     const baseURLMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
@@ -40,6 +14,7 @@ const localization = function(argv){
                 console.log(`${i}: ${response.body.features[i].place_name}`);
             });
         }
+
         else{
             //Darksky URL
             const baseURLDarksky = 'https://api.darksky.net/';
