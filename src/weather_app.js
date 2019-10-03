ce1d177b048ab35193b23effdf51913f5befbf5c
LauraRgz <lrodriguezs5@alumnos.nebrija.es>
2019-10-03 22:13:55 +0200

    weather_app.js

--- src/weather_app.js
+++ src/weather_app.js
@@ -1,5 +1,4 @@
 import request from 'request';
-export {localization};
 
 const localization = function(argv){
     //Mapbox URL
@@ -35,3 +34,4 @@ const localization = function(argv){
         }
     });
 }
+export{localization}
