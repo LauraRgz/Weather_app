7fa99d9ee83279d90be507c2ba8fbf8cea7af136
LauraRgz <lrodriguezs5@alumnos.nebrija.es>
2019-10-03 18:07:40 +0200

    comments

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ It shows a list of the localizations of the places with that name.
 Enter the command: `npm start -- localization --name="localization's name"`
 
 ### Name and Index
-It shows the time zone, temperature in ºC and the raining probability in the city with the index given in the list.  
+It shows the time zone, temperature in ℃ and the raining probability in the city with the index given in the list.  
 Enter the command: `npm start -- localization --name="madrid" --index="1"`
--- src/app.js
+++ src/app.js
@@ -28,31 +28,34 @@ request({ url: url, json: true }, (error, response) => {
 //https://api.mapbox.com/geocoding/v5/mapbox.places/vallecas.json?access_token=pk.eyJ1IjoibGF1cmFyZ3oiLCJhIjoiY2sxYW8xcG5uMTlldjNjbGw2aGNsYXZsNyJ9.yZNc4Be1agBMM6tVZ1uW-A
 
 const localization = function(argv){
+    //Mapbox URL
     const baseURLMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
     const name = argv.name;
     const token = '?access_token=pk.eyJ1IjoibGF1cmFyZ3oiLCJhIjoiY2sxYW8xcG5uMTlldjNjbGw2aGNsYXZsNyJ9.yZNc4Be1agBMM6tVZ1uW-A';
     const urlMapbox = `${baseURLMapbox}${name}.json${token}`;
-    
-
 
     request({ url: urlMapbox, json: true }, (error, response) => {
-        if (argv.index == null){
+        if (argv.index == null){ //No index given
             response.body.features.forEach((element,i) => {
                 console.log(`${i}: ${response.body.features[i].place_name}`);
             });
         }
         else{
+            //Darksky URL
             const baseURLDarksky = 'https://api.darksky.net/';
             const tokenDarksky = 'af414fe762e5e219154cd7dd41438b01/';
-            const lat = response.body.features[argv.index].geometry.coordinates[1];
+            //lat and long taken from Mapbox
+            const lat = response.body.features[argv.index].geometry.coordinates[1]; 
             const long = response.body.features[argv.index].geometry.coordinates[0];
             const parameters = '?units=si&lang=es';
             const urlDarksky = `${baseURLDarksky}forecast/${tokenDarksky}${lat},${long}${parameters}`;
-            console.log(lat + " " + long);
+            
+            const localizacion = response.body.features[argv.index].place_name; //Used to show all the localization information of the chosen city
+            
             request({ url: urlDarksky, json: true }, (error, response) => {
-                console.log("Buenas tardes, estás en " + response.body.timezone);
-                console.log("La temperatura es: " + response.body.currently.temperature); // La info siempre está en body
-                console.log("La probabilidad de precipitación es: " + response.body.currently.precipProbability);
+                console.log(`Temperatura y probabilidad de lluvia en: ${localizacion}`);
+                console.log(`La temperatura es: ${response.body.currently.temperature} ℃`); // La info siempre está en body
+                console.log(`La probabilidad de precipitación es: ${response.body.currently.precipProbability}`);
             });
         }
     });
